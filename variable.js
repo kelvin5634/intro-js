abee612082a837f3931d6505a9a821b4692275e3
kelvin34 <kelvinmwangi5634@gmail.com>
2025-02-26 11:14:54 +0300

    intro to js

---
+++ variable.js
@@ -0,0 +1,16 @@
+//variables allow storing data in the memory for use in program or software
+// in js we create / initialize variables using var, let, const
+// var is old way of creating variables\
+var age = 90
+// let ang const are modern/es6 way of creating variables
+let username = "kelvin"
+const interestRate = 30
+
+//let is used with values/ variables that can be changed as the program runs
+// cont is used for values that are constant and cannot be changed at any point in the program/software e.g PI
+const PI = 3.142
+
+// the values in variables cn be accessed using the variable name
+// console.log(something) is a a way for developers to print/ see the outcome in the terminal/console
+console.log(age)
+console.log(PI + interestRate)
